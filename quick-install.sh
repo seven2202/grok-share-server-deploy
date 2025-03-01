3a32357dd0680156163ecf0d737dd686cecb2d49
lyy0709 <2021011603@student.cup.edu.cn>
2025-03-01 16:22:20 +0800

    更新 README.md，添加快速部署说明和免责声明；修改 config.yaml 中的 JWT 密钥；优化 docker-compose.yml 中的环境变量注释；新增 quick-install.sh 脚本以简化部署流程

---
+++ quick-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+## 克隆仓库到本地
+echo "clone repository..."
+git clone -b deploy  --depth=1 https://github.com/lyy0709/grok-share-server-deploy.git grok-share
+
+## 进入目录
+cd grok-share
+
+docker compose pull
+docker compose up -d --remove-orphans
+
+## 提示信息
+echo "服务启动成功，请访问 http://localhost:8300"
+echo "管理员后台地址 http://localhost:8300/lyy0709"
+echo "管理员账号: admin"
+echo "管理员密码: 123456"
+echo "请及时修改管理员密码"
